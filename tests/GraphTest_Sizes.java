8ce44e2ec6e9347de23aa95520eb60d27d9b6dea
ShmueLa <shmuel516@gmail.com>
2020-11-15 21:30:11 +0200

    20201115 - #10 - Tests V2

---
+++ tests/GraphTest_Sizes.java
@@ -0,0 +1,29 @@
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.awt.*;
+
+public class GraphTest_Sizes {
+
+    @BeforeAll
+    static void g_creator() {
+    }
+
+    @Test
+    @DisplayName("Check Node Size")
+    void nodeSize_test() {
+        WGraph_DS g1 = new WGraph_DS();
+        for (int i=0; i<100; i++) {
+            g1.addNode(i);
+        }
+        assertEquals(100,g1.nodeSize());
+        g1.removeNode(99);
+        assertEquals(100-1,g1.nodeSize());
+        for (int i=0; i<25; i++) {
+            g1.removeNode(i);
+        }
+        assertEquals(100-1-25,g1.nodeSize());
+    }
+}
---
+++ tests/WGraph_DSTest_Methods.java
@@ -0,0 +1,54 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class WGraph_DSTest_Methods {
+
+    @Test
+    void getNode() {
+    }
+
+    @Test
+    void hasEdge() {
+    }
+
+    @Test
+    void getEdge() {
+    }
+
+    @Test
+    void addNode() {
+    }
+
+    @Test
+    void connect() {
+    }
+
+    @Test
+    void getV() {
+    }
+
+    @Test
+    void testGetV() {
+    }
+
+    @Test
+    void removeNode() {
+    }
+
+    @Test
+    void removeEdge() {
+    }
+
+    @Test
+    void nodeSize() {
+    }
+
+    @Test
+    void edgeSize() {
+    }
+
+    @Test
+    void getMC() {
+    }
+}
